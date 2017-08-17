307ff78821cb468d7220ef212cf62cae5e87a4c8
afeiship <1290657123@qq.com>
2017-08-17 11:01:40 +0800

    issue: intial

---
+++ dist/next-repeat.js
@@ -0,0 +1,17 @@
+(function () {
+
+  var global = global || window || self || this;
+
+  var nx = global.nx || require('next-js-core2');
+
+  nx.repeat = function(inChar,inCount){
+    var arr = new Array( inCount + 1 );
+    return arr.join(inChar);
+  };
+
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.repeat;
+  }
+
+}());
